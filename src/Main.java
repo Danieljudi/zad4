1b83d36ca9e6133e85c48a1493d63a3628d6080d
Danieljudi <41111907+Danieljudi@users.noreply.github.com>
2018-09-14 20:37:30 +0200

    switch-caseOption

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,35 @@
 public class Main {
     public static void main(String[] args)
     {
+        /**
        int a=8;
-    if(a<10)
+        if(a<10)
         System.out.println("tak jest, zmienna jest mniejsza od 10");
         else if (a==10)
         System.out.println("a=10");
         else
         System.out.println("a jest wieksze od 10");
+         */
+        int b=6;
+        switch(b)
+        {
+            case 1:
+                System.out.println("b=1");
+                break;
+            case 2:
+                System.out.println("b=2");
+                break;
+            case 3:
+                System.out.println("b=3");
+                break;
+            case 4:
+            System.out.println("b=4");
+                break;
+            case 5:
+            System.out.println("b=5");
+                break;
+                default:
+                    System.out.println("default");
+        }
     }
 }
